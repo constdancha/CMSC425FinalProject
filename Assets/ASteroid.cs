2f928ac5b257f9c9de8d7fb761bc91b7c52f9f39
Daniel Cha <Daniel@Daniels-MacBook-Pro.local>
2016-04-06 19:00:37 -0400

    First commit! Initial files for the project

---
+++ Assets/ASteroid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ASteroid : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		Vector3 movement = new Vector3 (0.0f, -1.0f, 0.0f);
+		rb.AddForce (movement * 1.0f);
+	}
+}
