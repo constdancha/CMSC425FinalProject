1e5b66bf28f534fcc96a5c4d6e8a205b00e46238
Ellaelle <hae18lee@gmail.com>
2016-04-11 21:40:19 -0400

    adding playerscript code and cleaning up playerhealth

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour {
 
 	private int health;
 	private int speed;
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	//need speed, health
+
+	private Rigidbody rb;
+	public int speed;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		rb.useGravity = false;
+		speed = 50;
+	}
+		
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 leftForce, rightForce, downForce, upForce;
+		int opposingForce = 5;
+
+		if (rb.position.x >= 8.70f) {
+			leftForce = new Vector3 (-3.0f, 0.0f, 0.0f);
+			rb.AddForce (leftForce * opposingForce);
+
+		} else if(rb.position.x <= -8.70f){
+			rightForce = new Vector3 (3.0f, 0.0f, 0.0f);
+			rb.AddForce (rightForce * opposingForce);
+
+		}else if(rb.position.y >= 4.40f){
+			downForce = new Vector3 (0.0f, -3.0f, 0.0f);
+			rb.AddForce (downForce * opposingForce);
+
+		}else if(rb.position.y <= -4.40f){
+			upForce = new Vector3 (0.0f, 3.0f, 0.0f);
+			rb.AddForce (upForce * opposingForce);
+		}
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		rb.AddForce (movement * speed * Time.deltaTime);
+	
+	}
+}
