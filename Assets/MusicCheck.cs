fce145ffcc9e4008d2ad8cd6ed3be4e4110b05c3
Ellaelle <hae18lee@gmail.com>
2016-05-17 19:03:16 -0400

    added background music

---
+++ Assets/MusicCheck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicCheck : MonoBehaviour {
+
+	public GameObject music;
+	// Use this for initialization
+	void Start () {
+		GameObject musicObject = GameObject.FindGameObjectWithTag ("GameController");
+		if(!musicObject){
+			Instantiate (music, Vector3.zero, Quaternion.identity);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
